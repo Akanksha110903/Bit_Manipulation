466518797371cd06451497f7f6d3e821609888cf
Akanksha <akanksha.bhaisare@atlascopco.com>
2026-03-11 08:53:30 +0530

    check if nth bit set

---
+++ first.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#define is_bit_set(num,pos)  ((num>>pos) & 1)
+
+int main() {
+    int num,pos;
+    printf("Enter the number and position to check: ");
+    scanf("%d %d",&num,&pos);
+    if(is_bit_set(num,pos))
+        printf("Bit is set at position %d\n",pos);
+    else
+        printf("Bit is not set at position %d\n",pos);
+
+    return 0;
+}
