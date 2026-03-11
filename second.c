c18ac602da5b79d4f5a3f9ec0eedef7ded44d575
Akanksha <akanksha.bhaisare@atlascopco.com>
2026-03-11 09:09:46 +0530

    set nth bit

---
+++ second.c
@@ -0,0 +1,12 @@
+//set a bit at a given position in a number
+#include <stdio.h>
+#define set_bit(num,pos) ((num) | (1U<<(pos)))
+
+int main(){
+        int num,pos;
+    printf("Enter the number and position to set: ");
+    scanf("%d %d",&num,&pos);
+    num=set_bit(num,pos);
+    printf("Number after setting bit at position %d is %d\n",pos,num);
+    return 0;
+}
